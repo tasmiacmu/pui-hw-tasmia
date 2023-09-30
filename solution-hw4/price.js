a439778b976d9444dd1f026e63cbfba167b76dc6
Tasmia Haq <tasmiahaq@Tasmias-MacBook-Pro.local>
2023-09-30 17:10:07 -0400

    Update price.js

--- solution-hw4/price.js
+++ solution-hw4/price.js
@@ -1,4 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () { //https://www.javascripttutorial.net/javascript-dom/javascript-domcontentloaded/
+
+// establish glaze and pack options
+
     let glaze_options = {
         "Keep original": 0,
         "Sugar milk": 0,
@@ -16,6 +19,8 @@ document.addEventListener("DOMContentLoaded", function () { //https://www.javasc
     let glaze_select = document.getElementById('glaze');
     let pack_select = document.getElementById('psize');
     let final_price = document.getElementById('totalprice');
+
+    // populate glaze and pack size
 
     for (let key in glaze_options) { // https://softauthor.com/javascript-dynamic-html-select-drop-down/
         let option = document.createElement("option");
@@ -37,20 +42,62 @@ document.addEventListener("DOMContentLoaded", function () { //https://www.javasc
         pack_select.appendChild(option);
     }
 
+
+    // url parameters
+
+    let cart = [];
+
+let query_string = window.location.search;
+let params = new URLSearchParams(query_string);
+let roll_type = params.get('roll');
+let current_roll = rolls[roll_type];
+
+// update header
+
+const header_element = document.querySelector('#productheader');
+header_element.innerText = roll_type + " Cinnamon Roll";
+
+// update image
+
+let roll_image = document.querySelector('.productimg img'); 
+    roll_image.src = '../assets/products/' + current_roll.imageFile;
+
+    // calculate price
+
     function calculate_price() {
         let glaze_change = parseFloat(glaze_select.value);
         let pack_change = parseFloat(pack_select.value);
+        let base_price = current_roll.basePrice;
 
-        let glaze_price = glaze_change + 2.49;
+        let glaze_price = glaze_change + base_price;
         let total_price = glaze_price * pack_change;
 
         final_price.textContent = "$" + total_price.toFixed(2); // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/toFixed
         // https://developer.mozilla.org/en-US/docs/Web/API/Node/textContent
     }
 
+    // update price on change
+
     calculate_price();
 
     glaze_select.onchange = calculate_price;
     pack_select.onchange = calculate_price;
+
+
+    // add roll to cart
+
+    class Roll {
+        constructor(roll_type, roll_glazing, size_pack, base_price) {
+            this.type = roll_type;
+            this.glazing =  roll_glazing;
+            this.size = size_pack;
+            this.basePrice = base_price;
+        }
+    }
+
+    let new_roll = new Roll; 
+
+
+    
 
 });
